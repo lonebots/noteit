690600b6915d725a9c74cb481cb0a1d0ada0386f
Jishnu <jishnusurajila@gmail.com>
2023-06-20 16:52:19 +0530

    feat: user login feature complete (#8)

--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { registerUserHandler } from '../controller/user.controller.js';
+import { loginUserHandler, registerUserHandler } from '../controller/user.controller.js';
+
 
 const userRouter = express.Router();
 
-userRouter.route('/register').post(registerUserHandler);
+userRouter.route('/register').post(registerUserHandler); //register route 
+userRouter.route('/login').post(loginUserHandler); // login
 
 export default userRouter;
