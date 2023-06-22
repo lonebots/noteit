3ae48baaaf7416ef6b0f69d88acb17b74a003f6c
lonebots <jishnusurajila@gmail.com>
2023-06-22 17:23:05 +0530

    chore: notes schema and model added

---
+++ server/src/model/note.model.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose"
+
+// schema
+const noteSchema = new mongoose.Schema({
+    title: { type: String, required: [true, 'Enter a title'] },
+    content: { type: String, required: [true, 'content is required'] },
+    user_id: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    username: { type: String, required: [true, 'User name is required'] },
+    date: { type: date, default: Date.now() },
+}, { timestamps: true })
+
+//model
+const NoteModel = new mongoose.model('Note', noteSchema)
+
+export default NoteModel;
