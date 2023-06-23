a81e5b70001a993c8d9f55ffa8d68987cc32ddd6
Jishnu ⚡ <jishnusurajila@gmail.com>
2023-06-23 19:48:30 +0530

    chore: note update route,controller and service added (#28)
    * chore: update note service added
    
    * chore: note update controller added
    
    * chore: update note route added

--- server/src/service/note.service.js
+++ server/src/service/note.service.js
@@ -1,7 +1,12 @@
 import noteModel from '../model/note.model.js'
+import ErrorResponse from '../utils/errorResponse.utils.js'
 
 // create note 
 export async function createNote(input) {
-    return await noteModel.create(input)   
+    return await noteModel.create(input)
 }
 
+// find and update 
+export async function findByIdAndUpdate(id, updatedObject) {
+    return await noteModel.findByIdAndUpdate(id, updatedObject)
+}
