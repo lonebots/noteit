15b5a8ee6c254c387881e1e6814507f8fc3e0a62
lonebots <jishnusurajila@gmail.com>
2023-06-23 19:46:29 +0530

    chore: update note route added

--- server/src/routes/note.route.js
+++ server/src/routes/note.route.js
@@ -1,10 +1,13 @@
 import express from 'express'
-import { createNoteHandler } from '../controller/note.controller.js'
+import { createNoteHandler, updateNoteHandler } from '../controller/note.controller.js'
 import auth from '../middleware/auth.middleware.js'
 
 const noteRouter = express.Router()
 
 // routes 
 noteRouter.route('/').post(auth, createNoteHandler)
+
+// crud
+noteRouter.route('/:id').put(auth, updateNoteHandler)
 
 export default noteRouter
