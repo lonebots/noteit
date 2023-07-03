bdc68d2bdaac6ece80e27c16bb70fdaff8655bfd
Jishnu ⚡ <jishnusurajila@gmail.com>
2023-07-03 22:39:13 +0530

    fix: server url updated

--- client/src/API/Url.js
+++ client/src/API/Url.js
@@ -1,3 +1,3 @@
 // const url = 'http://localhost:4000'
-const url = 'https://test-server-kappa-five.vercel.app'
+const url = 'https://noteit-server-4ec4mvuzl-lonebots.vercel.app/'
 export default url;
