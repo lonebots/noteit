f210e48170f89c9c4a309c8f9ac3809fda575db4
lonebots <jishnusurajila@gmail.com>
2023-07-03 15:12:02 +0530

    fix: redirect fix after login to home

--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import './Home.css'
 import noteAnimate from '../Assets/note-animation.gif'
 
 function Home({ }) {
+    const isLogged = localStorage.getItem('is-logged');
+    const navigate = useNavigate()
+    useEffect(() => {
+        function navgateBacktoDash(isLogged) {
+            if (isLogged) {
+                navigate('/dash');
+            }
+        }
+        navgateBacktoDash(isLogged)
+    }, [isLogged])
+
     return (
         <div className='home-container'>
             <h1 className='hero'>Your Ultimate <span>Note-Making  </span> Companion!</h1>
