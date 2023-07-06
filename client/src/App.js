224ccbbb75482dc1e07b634ecd83ce2c7addd992
lonebots <jishnusurajila@gmail.com>
2023-07-06 13:10:15 +0530

    chore: Made Nav and Home responsive

--- client/src/App.js
+++ client/src/App.js
@@ -49,8 +49,7 @@ function App() {
   }, [])
 
   return (
-    <div className="App">
-
+    <div className='App'>
       <Nav isLogged={isLogged} setIsLogged={setIsLogged} />
       <div className='main'>
         <Routes>
